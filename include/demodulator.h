fa6bafaf2d3050581120202e28a170a2ac279224
Felix Morgner <felix.morgner@hsr.ch>
2016-05-23 16:37:00 +0200

    Added some steps

--- include/demodulator.h
+++ include/demodulator.h
@@ -4,8 +4,8 @@
 #include <types/common_types.h>
 #include <types/transmission_mode.h>
 
-#include <array>
 #include <cstdint>
+#include <vector>
 
 namespace dab
   {
@@ -18,13 +18,18 @@ namespace dab
 
     private:
       __internal_common::sample_t get_sample(std::int32_t const phaseShift);
+      void get_samples(std::vector<__internal_common::sample_t> & target, std::int16_t count, std::int32_t phaseShift);
 
       __internal_common::types::transmission_mode const m_mode;
       sample_queue_t & m_sampleQueue;
 
       std::int32_t m_phaseShift{};
       std::vector<__internal_common::sample_t> m_oscilator{};
+      std::vector<__internal_common::sample_t> m_symbolBuffer{};
       float m_signalLevel{};
+
+      std::int16_t m_fineCorrection{};
+      std::int32_t m_coarseCorrection{};
     };
 
   }
--- src/demodulator.cpp
+++ src/demodulator.cpp
@@ -3,6 +3,10 @@
  */
 #include "demodulator.h"
 
+#include <constants/sample_rate.h>
+#include <literals/binary_literal.h>
+
+#include <array>
 #include <cmath>
 
 namespace dab
@@ -15,17 +19,88 @@ namespace dab
     : m_mode{mode},
       m_sampleQueue{samples}
     {
-    m_oscilator.resize(2048000);
+    m_oscilator.resize(kDefaultSampleRate);
 
     for(std::size_t index{}; index < m_oscilator.size(); ++index)
       {
       m_oscilator[index] = sample_t(cos(2.0f * M_PI * index / m_oscilator.size()), sin(2.0 * M_PI * index / m_oscilator.size()));
       }
+
+    m_symbolBuffer.resize(m_mode.frame_symbols + 1 * m_mode.symbol_duration);
     }
 
   void demodulator::operator()()
     {
+    using namespace __internal_common::literals;
+    auto constexpr bufferMask = 1111111111111111_b;
 
+    auto bufferIndex = 0;
+    auto currentSignalLevel = 0.0f;
+
+    for(auto sampleIdx = 0; sampleIdx < 10 * m_mode.symbol_duration; ++sampleIdx)
+      {
+      auto sample = get_sample(0);
+      currentSignalLevel += abs(sample);
+      }
+
+    m_signalLevel = currentSignalLevel / (10 * m_mode.symbol_duration);
+    std::array<float, bufferMask + 1> envelopeBuffer{};
+
+notSynced:
+
+    for(auto sampleIdx = 0; sampleIdx < m_mode.symbol_duration; ++sampleIdx)
+      {
+      auto sample = get_sample(0);
+      envelopeBuffer[bufferIndex] = std::abs(sample);
+      bufferIndex = (bufferIndex + 1) & bufferMask;
+      }
+
+    auto currentStrength = 0.0f;
+
+    for(auto idx = bufferIndex - 50; idx < bufferIndex; ++idx)
+      {
+      currentStrength += envelopeBuffer[idx];
+      }
+
+    auto tries = 0;
+
+    while(currentStrength / 50 > 0.4f * m_signalLevel)
+      {
+      auto sample = get_sample(m_coarseCorrection + m_fineCorrection);
+      envelopeBuffer[bufferIndex] = std::abs(sample);
+      currentStrength += envelopeBuffer[bufferIndex] - envelopeBuffer[(bufferIndex + envelopeBuffer.size() - 50) & bufferMask];
+      bufferIndex = (bufferIndex + 1) & bufferMask;
+      ++tries;
+
+      if(tries > 2 * m_mode.symbol_duration)
+        {
+        goto notSynced;
+        }
+      }
+
+    while(currentStrength / 50 < 0.75f * m_signalLevel)
+      {
+      auto sample = get_sample(m_coarseCorrection + m_fineCorrection);
+      envelopeBuffer[bufferIndex] = std::abs(sample);
+      currentStrength += envelopeBuffer[bufferIndex] - envelopeBuffer[(bufferIndex + envelopeBuffer.size() - 50) & bufferMask];
+      bufferIndex = (bufferIndex + 1) & bufferMask;
+      ++tries;
+      if(tries > 3 * m_mode.symbol_duration)
+        {
+        goto notSynced;
+        }
+      }
+
+//syncOnPhase:
+
+    for(auto sampleIdx = 0; sampleIdx < m_mode.useful_duration; ++sampleIdx)
+      {
+      m_symbolBuffer[sampleIdx] = get_sample(m_coarseCorrection + m_fineCorrection);
+      envelopeBuffer[bufferIndex] = std::abs(m_symbolBuffer[sampleIdx]);
+      currentStrength += envelopeBuffer[bufferIndex] - envelopeBuffer[(bufferIndex + envelopeBuffer.size() - 50) & bufferMask];
+      bufferIndex = (bufferIndex + 1) & bufferMask;
+      ++tries;
+      }
     }
 
   sample_t demodulator::get_sample(std::int32_t const phaseShift)
@@ -33,11 +108,22 @@ namespace dab
     sample_t sample{};
     m_sampleQueue.wait_dequeue(sample);
 
-    m_phaseShift = (m_phaseShift - phaseShift + 2048000) % 2048000;
+    m_phaseShift = (m_phaseShift - phaseShift + kDefaultSampleRate) % kDefaultSampleRate;
     sample *= m_oscilator[m_phaseShift];
     m_signalLevel = 0.00001 * std::abs(sample) + (1 - 0.00001) * m_signalLevel;
 
     return sample;
+    }
+
+  void demodulator::get_samples(std::vector<sample_t> & target, std::int16_t count, std::int32_t phaseShift)
+    {
+    for(std::int16_t iteration{}; iteration < count; ++count)
+      {
+      m_sampleQueue.wait_dequeue(target[iteration]);
+      m_phaseShift = (m_phaseShift - phaseShift + kDefaultSampleRate) % kDefaultSampleRate;
+      target[iteration] *= m_oscilator[m_phaseShift];
+      m_signalLevel = 0.00001 * std::abs(target[iteration]) + (1 - 0.00001) * m_signalLevel;
+      }
     }
 
   }
